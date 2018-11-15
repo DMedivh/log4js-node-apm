48be3ec2e568891ea21e5698685ddc577d213906
DingGuofeng <dingguofeng@taoziyouxi.net>
2018-11-15 17:40:31 +0800

    add files

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+// Some types refer to https://github.com/log4js-node/log4js-node/blob/master/types/log4js.d.ts
+export interface LogstashUDPAppender {
+    'type': '@log4js-node/apm';
+    // hostname (or IP-address) of the logstash server
+    host: string;
+    // port of the logstash server
+    port: number;
+    // used for the message field of the logstash data
+    // the layout should be the `Layout` type in log4js
+    layout?: object;
+    // used to customize the log object being sent to logstash UDP server
+    // the event should be the `LoggingEvent` type in log4s
+    extraDataProvider?: (event: object) => object;
+}
